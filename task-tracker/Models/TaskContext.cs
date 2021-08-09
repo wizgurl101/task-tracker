ab446e689abb8fc539bc5a0e1f263403ddce8695
wizgurl101 <nb.phan@hotmail.com>
2021-08-09 07:11:15 -0600

    Add project files.

---
+++ task-tracker/Models/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace task_tracker.Models
+{
+    // the : colon depict class inheritance
+    // a base class is specified by appending a colon and the name of the base class
+    // following the derived class name
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
---
+++ task-tracker/Models/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task_tracker.Models
+{
+    public class TaskItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompete { get; set; }
+    }
+}
